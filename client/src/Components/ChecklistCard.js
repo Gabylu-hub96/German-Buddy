00395313073bf6b9971aa9a8a94286bda889bab7
Gabriela Andrade Juarez <gabyluchisjuarez@gmail.com>
2023-03-31 20:23:45 +0200

    delete checklist working

--- client/src/Components/ChecklistCard.js
+++ client/src/Components/ChecklistCard.js
@@ -3,19 +3,34 @@ import axios from "../axiosInstance";
 import { useParams, useNavigate, Link } from "react-router-dom";
 
 const CheckListCard = (props) => {
-  const { id } = useParams();
-
   const navigate = useNavigate();
   const [checkList, setCheckList] = useState(props.checkList);
-  const handleDelete = () => {
+
+  // this deletes the checkList
+  const handleDeleteCheckList = () => {
     axios
-      .delete(`/api/checkLists/${id}`)
-      .then((res) => navigate("/"))
+      .delete("/api/checkList/" + checkList._id)
+      .then(
+        (res) => navigate(0) // this reloads the myChecklists page (this checkList will disappear)
+      )
       .catch((e) => console.log(e));
   };
+
+  // // this updates the checkList
+  // const handleSaveCheckList = () => {
+  //   axios
+  //     .update("/api/checkList/" + checkList._id)
+  //     .then((res) => {})
+  //     .catch((e) => console.log(e));
+  // };
+
   return (
     <>
       <div className="checkList">
+        <div>
+          <button onClick={handleDeleteCheckList}>delete</button>
+          <button onClick={handleSaveCheckList}>save</button>
+        </div>
         <h1>{checkList.title}</h1>
         {checkList.tasks.map((task) => {
           return (

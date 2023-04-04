c834cd50a6c6bde0b2921a8bfa4a5fb309cffa40
Gabriela Andrade Juarez <gabyluchisjuarez@gmail.com>
2023-04-04 09:58:17 +0200

    progress bar working for documents

--- client/src/Components/ChecklistCard.js
+++ client/src/Components/ChecklistCard.js
@@ -1,19 +1,24 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import axios from "../axiosInstance";
+import ProgressBar from "react-bootstrap/ProgressBar";
+
 const CheckListCard = (props) => {
   const navigate = useNavigate();
   const [checkList, setCheckList] = useState(props.checkList);
+  const [docsPercentage, setDocsPercentage] = useState(
+    handleProgressBar(props.checkList.documents)
+  );
 
   const handleChange = (myDoc) => {
     const newDocuments = checkList.documents.map((doc) => {
-      console.log("WHAT IS THIS???");
       if (doc._id === myDoc._id) {
         doc.isCompleted = !doc.isCompleted;
       }
       return doc;
     });
     setCheckList({ ...checkList, documents: newDocuments });
+    setDocsPercentage(handleProgressBar(newDocuments));
     axios
       .put(`/api/checkList/${checkList._id}`, {
         ...checkList,
@@ -22,6 +27,15 @@ const CheckListCard = (props) => {
       .then((res) => console.log(res))
       .catch((e) => console.log(e));
   };
+
+  function handleProgressBar(docs) {
+    const progress = docs.reduce((acc, curr) => {
+      console.log("current", curr.isCompleted);
+      return (acc += curr.isCompleted ? 1 : 0);
+    }, 0);
+    console.log("progress", progress);
+    return (progress / docs.length) * 100;
+  }
 
   return (
     <>
@@ -40,9 +54,15 @@ const CheckListCard = (props) => {
                 {document.title}
               </h4>
               <p>{document.description}</p>
+              <div> {document.progress} </div>
             </div>
           );
         })}
+        <ProgressBar
+          animated
+          now={docsPercentage}
+          label={`${Math.ceil(docsPercentage)}%`}
+        />
         <h1>Checklist of necessary steps for {checkList.category}</h1>
         {checkList.steps.map((step) => {
           return (

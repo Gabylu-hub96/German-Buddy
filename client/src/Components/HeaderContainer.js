6200c92a9414432450a8303787d2d26f5750ad45
KimsandraO <kimsandra08@gmail.com>
2023-04-05 13:24:41 +0200

    hero updated

--- client/src/Components/HeaderContainer.js
+++ client/src/Components/HeaderContainer.js
@@ -6,22 +6,22 @@ const HeaderContainer = () => {
   return (
     <div className="text-con">
       <div className="left-column">
-        <img
-          src={Logo}
-          alt="Logo"
-
-          className="d-inline-block"
-        />
+        <img src={Logo} alt="Logo" className="d-inline-block" />
         <p> We help make your settling in Germany uncomplicated.</p>
-        <button>Register Now</button>
+        <button>FIND OUR MORE</button>
       </div>
       <div className="image-container">
         <img
+          className="right-image"
           src="https://media-public.canva.com/YbKEU/MAE-U0YbKEU/1/tl.png"
+          col-md-6
+          float-md-end
+          mb-3
+          ms-md-3
           alt=".."
         />
-        </div>
       </div>
+    </div>
   );
 };
 
